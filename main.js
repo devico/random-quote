266ede0b25e4fac9e5cc824f06d38c33f7297457
Dmytrakov Serhii <admin@192.168.0.102.local.ipnet.ua>
2017-11-19 17:02:39 +0200

    Add function getRandomQuote()

---
+++ main.js
@@ -0,0 +1,17 @@
+let fetch = require('node-fetch')
+
+let getRandomQuote = () => {
+  return (
+	  fetch('https://talaikis.com/api/quotes/')
+		  .then(response => {
+		    return response.json()
+		  })
+		  .then(data => {
+		  	// data[Math.floor(Math.random() * data.length)].quote
+		    console.log(data[Math.floor(Math.random() * data.length)].quote)
+		  })
+		  .catch(ex => {
+		    console.log('parsing failed', ex)
+		  })
+	)
+}
